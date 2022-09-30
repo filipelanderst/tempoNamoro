1d5b8953e6bb7f7ae0c49037da7f7cc6374769be
Filipe Lander <flxcode7@onmail.com>
2022-09-30 18:53:39 -0300

    alteracoesGerais

--- src/js/script.js
+++ src/js/script.js
@@ -9,15 +9,14 @@ function relogioNamoro() {
   const minutos = new Date(hoje - dataNamoro).getMinutes();
   const segundos = new Date(hoje - dataNamoro).getSeconds();
 
-  const dataCorrida = `${anos} anos:${meses}:${dias}:${horas}:${minutos}:${segundos}`;
-  const miliPorDias = 864000000;
-  const namoroEmDias = (dataNamoro / miliPorDias - 49.4).toFixed();
+  const miliPorDias = 86400000;
+  const namoroEmDias = Math.floor((hoje - dataNamoro) / miliPorDias);
 
   const txtDias = document.querySelector('.num-dias');
   txtDias.innerHTML = namoroEmDias;
 
   const texto = document.querySelector('.texto');
-  texto.innerHTML = `Hoje está fazendo <span>${anos}</span> anos, <span>${meses}</span> meses, <span>${dias}</span> dias, <span>${horas}</span> horas, <span>${minutos}</span> minutos e <span>${segundos}</span> segundos que a gente está junto`;
+  texto.innerHTML = `<span>${anos}</span> anos, <span>${meses}</span> meses, <span>${dias}</span> dias, <span>${horas}</span> horas, <span>${minutos}</span> minutos e <span>${segundos}</span>s`;
 }
 
 setInterval(relogioNamoro, 1000);
